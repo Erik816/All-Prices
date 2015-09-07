6f2a689a52faa3b1c14b901df60c4bec15beb76d
Erik <Erik.Christensen@gmail.com>
2015-09-07 14:24:01 -0700

    created Chrome version

---
+++ Chrome/background.js
@@ -0,0 +1,33 @@
+function onWebNav(details) {
+    if (details.frameId === 0) {
+        // Top-level frame
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var filter = {
+    url: [{
+        hostContains: 'amazon.com'
+    }]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
+
+chrome.pageAction.onClicked.addListener(function(activeTab) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    console.log(url);
+
+    var regex = new RegExp("http://www.amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})");
+
+    asin = url.match(regex);
+
+    if (asin) {
+      chrome.tabs.create({url: "http://www.amazon.com/gp/offer-listing/" + asin[4]});
+    }
+    else {
+      notifications.notify({
+        text: "Sorry, no price information found!",
+      });
+    };
+  });
+});
